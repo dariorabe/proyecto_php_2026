012b551b2d652f38b292bab648e7d9d57887d388
Dario Rabe <dario.rabe@galileo.edu>
2026-03-16 00:40:36 +0000

    Agregamos código e imagen

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo</title>
+
+    <style>
+        body{
+	    text-align: center;
+	    font-family: Arial, sans-serif;
+	    background-color: #f2f2f2;
+	}
+
+	img{
+	    margin-top: 20px;
+	    border-radius: 10px;
+	}
+    </style>
+
+</head>
+
+<body>
+
+<h1>Hola mundo, soy Darío Alfredo Rabe Godoy</h1>
+
+<img src="imagen_ai.jpg" width="400">
+
+</body>
+</html>
